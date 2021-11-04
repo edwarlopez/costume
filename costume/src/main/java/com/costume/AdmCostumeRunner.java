70a8800feb31d73bd710ce6ebc7dc7ce2bd28bef
edwardlopez <edwar.lopez.mt@correo.usa.edu.co>
2021-11-04 17:02:39 -0500

    HABER SI POR FIN FUNCIIONA

---
+++ costume/src/main/java/com/costume/AdmCostumeRunner.java
@@ -0,0 +1,19 @@
+package com.costume;
+
+import com.costume.repository.crud.ReservationCrudRepository;
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ *
+ * @author desaextremo
+ */
+public class AdmCostumeRunner implements CommandLineRunner {
+    
+    private ReservationCrudRepository reservationCrudRepository;
+    
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+    
+}
---
+++ costume/src/main/java/com/costume/model/custom/CountClient.java
@@ -0,0 +1,18 @@
+package com.costume.model.custom;
+
+import com.costume.model.Client;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author desaextremo
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountClient {
+    private Long total;
+    private Client client;
+}
